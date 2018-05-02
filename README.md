f0b8c65f7661bba5e37cbc93469156d0e70acc1c
AnitaMartinez <amartinezaguilar6@gmail.com>
2018-05-02 21:22:01 +0200

    Installed http-server and fetch local data

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Estudiantes de informática 
+## Comparación entre el número de mujeres y el número de hombres
+### Visualización de datos
+
+Instalar y arrancar: 
+
+ ```npm install http-server -g
+
+ http-server
+ ```
+
+
--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+//run http-server
+
+function csvJSON(csv) {
+    const lines = csv.split("\n");
+    const result = [];
+    const headers = lines[0].split(";");
+    for (let i = 1; i < lines.length; i++) {
+        const obj = {};
+        const currentline = lines[i].split(";");
+        for (let j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+        }
+        result.push(obj);
+    }
+    return result;
+}
+
+fetch("http://localhost:8080/csv/2016-2017.csv")
+    .then((response) => {
+        return response.text();
+    })
+    .then((text) => {
+        return csvJSON(text);
+    })
+    .then((json) => {
+        console.log(json);
+    })
+
