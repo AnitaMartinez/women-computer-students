628dfa78f98619a28aebb0c75f2428a031171de1
Ana Martínez <amartinezaguilar6@gmail.com>
2018-11-03 18:23:10 +0100

    Added functionality to male and female buttons

--- script/main.js
+++ script/main.js
@@ -1,23 +1,29 @@
 (function () {
     'use strict';
-
     const ctx = document.getElementById("chart").getContext('2d');
+    let myChart;
+    let studentsPerYear = [];
+    const idDataset = {
+        women : 'women',
+        men   : 'men'
+    };
 
-    const createChart = studentsPerYear => {
+    const createChart = () => {
         const chartData = {
             labels: studentsPerYear.year,
             datasets: [{
                 backgroundColor: '#9900ff',
                 stack: 'Stack 0',
-                data: studentsPerYear.women
+                data: studentsPerYear.women,
+                idDataset : 'women'
             }, {
                 backgroundColor: '#1FC3AA',
                 stack: 'Stack 0',
-                data: studentsPerYear.men
+                data: studentsPerYear.men,
+                idDataset : 'men'
             }]
         };
-
-        const myChart = new Chart(ctx, {
+        myChart = new Chart(ctx, {
             type: 'bar',
             data: chartData,
             options: {
@@ -45,44 +51,62 @@
                             fontColor : "#2E2E2E"
                         }
                     }]
-                },
-                hover : {
-                    /*onHover : e => {
-                        const chart = document.getElementById('chart');
-                        chart.style.cursor = "pointer";
-                    }*/
                 }
             }
         });
     };
 
+    const updateChartByGender = (idDatasetToDelete, idDatasetToAdd) => {
+        let datasetToDelete = myChart.data.datasets.find(dataset => dataset.idDataset === idDatasetToDelete);
+        let datasetToAdd = myChart.data.datasets.find(dataset => dataset.idDataset === idDatasetToAdd);
+        datasetToDelete.data = [];
+        if(datasetToAdd.data.length === 0) {
+            datasetToAdd.data = studentsPerYear[idDatasetToAdd];
+        }
+        myChart.update();
+    };
+
+    const resetChart = () => {
+        myChart.data.datasets.find(dataset => dataset.idDataset === idDataset.women).data = studentsPerYear.women;
+        myChart.data.datasets.find(dataset => dataset.idDataset === idDataset.men).data = studentsPerYear.men;
+        myChart.update();
+    };
+
+    const onWomenBtn = () => {
+        updateChartByGender(idDataset.men, idDataset.women);
+    };
+
+    const onMenBtn = () => {
+        updateChartByGender(idDataset.women, idDataset.men);
+    };
+
+    const onGenderButtons = () => {
+        const womenBtn = document.getElementById('btnWomen');
+        const menBtn = document.getElementById('btnMen');
+        const resetBtn = document.getElementById('btnReset');
+        womenBtn.addEventListener('click', onWomenBtn);
+        menBtn.addEventListener('click', onMenBtn);
+        resetBtn.addEventListener('click', resetChart);
+    };
+
     getDataStudents()  //ES6 ??
-        .then(studentsPerYear => {
+        .then(dataStudents => {
             //NO SÉ SI ESTO VA A AQUÍ O EN DATA.JS
-            studentsPerYear.shift(); //temporal, por el NaN
-            const studentsPerYearOrdered = {women: [], men: [], year: []};
-            studentsPerYear.forEach(studentsOneYear => {
+            dataStudents.shift(); //temporal, por el NaN
+            studentsPerYear = {women: [], men: [], year: []};
+            dataStudents.forEach(studentsOneYear => {
                 let women = 0;
                 let men = 0;
                 studentsOneYear.forEach(students => {
                     women += students.women;
                     men += students.men;
                 });
-                studentsPerYearOrdered.women.push(women);
-                studentsPerYearOrdered.men.push(men);
-                studentsPerYearOrdered.year.push(studentsOneYear[0].schoolYearInitial);
+                studentsPerYear.women.push(women);
+                studentsPerYear.men.push(men);
+                studentsPerYear.year.push(studentsOneYear[0].schoolYearInitial);
             });
-            console.log('studentsPerYearOrdered->', studentsPerYearOrdered);
-
-            createChart(studentsPerYearOrdered);
-
-
-            const womenBtn = document.getElementById('btnWomen');
-
-            womenBtn.addEventListener('click', () => {
-                console.log('pulsaste');
-            })
-
+            createChart();
+            onGenderButtons();
     });
 
 })();
